dfabf7c34988fe0f9979fc213e8cf7facee867f8
Jorge Matricali <jorgematricali@gmail.com>
2019-03-06 14:44:26 -0300

    Commit inicial

---
+++ src/main/java/ar/net/argentum/cliente/Cliente.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2019 Jorge Matricali <jorgematricali@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package ar.net.argentum.cliente;
+
+import ar.net.argentum.cliente.gui.PanelCargando;
+import ar.net.argentum.cliente.gui.PanelConectar;
+import ar.net.argentum.cliente.gui.PanelJugar;
+import ar.net.argentum.cliente.gui.VentanaPrincipal;
+import ar.net.argentum.cliente.protocolo.ConexionConServidor;
+import java.awt.CardLayout;
+import java.io.IOException;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jorge Matricali <jorgematricali@gmail.com>
+ */
+public class Cliente {
+
+    private static Cliente instancia;
+
+    public static void main(String[] args) throws IOException {
+        Cliente cliente = Cliente.getCliente();
+    }
+
+    public static Cliente getCliente() {
+        if (null == instancia) {
+            instancia = new Cliente();
+        }
+        return instancia;
+    }
+
+    private final VentanaPrincipal ventana;
+    private final JPanel panelPadre;
+    private final PanelConectar panelConectar;
+    private final PanelJugar panelJugar;
+    private final PanelCargando panelCargando;
+    private final CardLayout layout;
+    private ConexionConServidor conexion = null;
+
+    private Cliente() {
+        // Creamos un nuevo panel padre
+        this.layout = new CardLayout();
+        this.panelPadre = new JPanel(layout);
+
+        // Creamos un nuevo panel de conectar
+        this.panelConectar = new PanelConectar();
+
+        // Creamos un nuevo panel de juego
+        this.panelJugar = new PanelJugar();
+
+        // Creamos la pantalla de carga
+        this.panelCargando = new PanelCargando();
+
+        // Agregamos todos los paneles al panel padre
+        panelPadre.add(panelConectar, "conectar");
+        panelPadre.add(panelJugar, "jugar");
+        panelPadre.add(panelCargando, "cargando");
+
+        // Creamos la ventana principal
+        this.ventana = new VentanaPrincipal();
+
+        // Agregamos el panel padre a la ventana principal
+        ventana.add(panelPadre);
+
+        // Mostramos la ventana
+        ventana.setVisible(true);
+    }
+
+    public void conectar(String direccion, int puerto, String username, String password) {
+        System.out.println("Conectando...");
+        mostrarPanel("cargando");
+        this.conexion = new ConexionConServidor(direccion, puerto, username, password);
+        conexion.start();
+    }
+
+    public VentanaPrincipal getVentanaPrincipal() {
+        return ventana;
+    }
+
+    public PanelJugar getPanelJugar() {
+        return panelJugar;
+    }
+
+    public void mostrarPanel(String nombre) {
+        layout.show(panelPadre, nombre);
+    }
+
+    public ConexionConServidor getConexion() {
+        return conexion;
+    }
+
+}
---
+++ src/main/java/ar/net/argentum/cliente/protocolo/ConexionConServidor.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2019 Jorge Matricali <jorgematricali@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package ar.net.argentum.cliente.protocolo;
+
+import ar.net.argentum.cliente.Cliente;
+import ar.net.argentum.cliente.gui.GUI;
+import java.awt.HeadlessException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jorge Matricali <jorgematricali@gmail.com>
+ */
+public class ConexionConServidor extends Thread {
+
+    private Socket socket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+
+    private boolean corriendo = false;
+    private final String direccion;
+    private final int puerto;
+    private final String usuario;
+    private final String password;
+    private final byte version = 0x1;
+
+    public ConexionConServidor(String direccion, int puerto, String usuario, String password) {
+        this.direccion = direccion;
+        this.puerto = puerto;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Intentando conectar a " + direccion + ":" + puerto + "...");
+            // Resolver direccion
+            InetAddress ip = InetAddress.getByName(direccion);
+
+            // Establecemos la coneccion al puerto indicado
+            try {
+                this.socket = new Socket(ip, puerto);
+            } catch (ConnectException ex) {
+                // Volvemos a la pantalla de conectar
+                Cliente.getCliente().mostrarPanel("conectar");
+
+                // Mostramos mensaje de error
+                GUI.mostrarMensaje(ex.getMessage(), "No se pudo conectar al servidor");
+
+                // Ya no hay nada que hacer
+                return;
+            }
+
+            // Obtenemos los Streams de entrada y de salida
+            this.dis = new DataInputStream(socket.getInputStream());
+            this.dos = new DataOutputStream(socket.getOutputStream());
+
+            this.corriendo = true;
+            System.out.println("Conectado!");
+
+            // Iniciamos sesion
+            System.out.println("Iniciandooo sesion");
+            dos.writeByte(0x2); // Paquete INICIAR_SESION
+            dos.writeByte(version); // Version del protocolo
+            dos.writeUTF(usuario);
+            dos.writeUTF(password);
+
+            // Mostramos el panel de juego
+            Cliente.getCliente().mostrarPanel("jugar");
+
+            // Este bucle se encarga de intercambiar la informacion con el servidor
+            while (corriendo) {
+
+                // obtenemos el tipo de paquete recibido
+                Byte tipoPaquete = dis.readByte();
+
+                switch (tipoPaquete) {
+                    case 0x1:
+                        // Desconectar
+                        String mensaje = dis.readUTF();
+                        if (mensaje.isEmpty()) {
+                            mensaje = "Has sido desconectado del servidor. Razon desconocida.";
+                        }
+                        GUI.mostrarMensaje(mensaje, "Has sido desconectado");
+                        this.corriendo = false;
+                        break;
+
+                    case 0x3:
+                        // Mensaje de chat
+                        Cliente.getCliente().getPanelJugar().agregarMensajeConsola(dis.readUTF());
+                        break;
+
+                    default:
+                        String received = dis.readUTF();
+                        Cliente.getCliente().getPanelJugar().agregarMensajeConsola(received);
+                        System.out.println(received);
+                }
+            }
+
+            // Cerramos el socket
+            socket.close();
+
+            // Cerramos los recursos abiertos
+            dis.close();
+            dos.close();
+        } catch (HeadlessException | IOException e) {
+            Logger.getLogger(ConexionConServidor.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        // Volvemos a la pantalla de conectar
+        Cliente.getCliente().mostrarPanel("conectar");
+    }
+
+    public void enviarChat(String mensaje) {
+        try {
+            dos.writeByte(0x3); // CHAT
+            dos.writeUTF(mensaje);
+        } catch (IOException ex) {
+            Logger.getLogger(ConexionConServidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
