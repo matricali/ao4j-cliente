c33ef34a7d7643e2cfe2e09d27a37938aa1b60c3
Jorge Matricali <jorgematricali@gmail.com>
2019-03-25 01:36:06 -0300

    Saco el auto conectar que utilizo cuando estoy haciendo pruebas.

--- src/main/java/ar/net/argentum/cliente/motor/MotorGrafico.java
+++ src/main/java/ar/net/argentum/cliente/motor/MotorGrafico.java
@@ -247,7 +247,6 @@ public class MotorGrafico {
 
         this.corriendo = true;
         cliente.setJugando(false);
-        cliente.conectar("localhost", 7666, "rasta", "1");
 
         loop();
         destruir();
